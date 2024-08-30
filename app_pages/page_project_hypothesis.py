7a8450611532086d409139758a844ebd62429224
Joel Chan <ic.chanjoel@gmail.com>
2024-08-30 11:24:14 +0000

    modify hypothesis section in readme file

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def page_project_hypothesis_body():
-    st.write("### Hypotesis 1 and validation")
+    st.write("### Hypothesis 1 and validation")
 
     st.success(
         f"There are distinct visual differences between healthy cherry leaves and those affected by powdery mildew that can be identified through image analysis."
@@ -21,48 +21,34 @@ def page_project_hypothesis_body():
     )
 
 
-    st.write("### Hypotesis 2 and validation")
+    st.write("### Hypothesis 2 and validation")
 
     st.success(
-        f"Comparison of Mathematical Functions - The `softmax` function performs better than the `sigmoid` function as an activation function for the CNN output layer. "
+        f"Comparison of Mathematical Functions - The `` function performs better than the `` function as an activation function for the CNN output layer. "
     )
 
     st.info(
-        f"Both ```softmax``` and ```sigmoid``` are typically used as functions for binary or multi class classification problems."
-        f" How an activation function performs on a model can be evaluated by plotting the model's prediction capacity."
-        f" The learning curve shows the accuracy and error rate on the training and validation dataset while the model is training.\n\n"
-        f" The model trained using ```softmax``` showed less training/validation sets gap and more"
-        f" consistent learning rate after the 5th Epoch compared to the model trained using ```sigmoid```."
+        f""
     )
     st.warning(
         f"```softmax``` performed better than ```sigmoid```. "
+        f"```sigmoid``` performed better than ```softmax```. "
     )
-    model_perf_softmax = plt.imread(f"")
-    st.image(model_perf_softmax, caption='') 
-    model_perf_sigmoid = plt.imread(f"")
-    st.image(model_perf_sigmoid, caption='')
+    
 
-
-    st.write("### Hypotesis 3 and validation")
+    st.write("### Hypothesis 3 and validation")
 
     st.success(
         f"Use of ```RGB``` images for classification - Cherry leaf images converted from ```grayscale``` to ```RGB``` allow for an improved image classification performance. "
     )
     st.info(
-        f"Color digital images are made of pixels, and pixels are made of combinations of primary colors."
-        f" Grayscale images, are black-and-white and each pixel is a single sample representing only an amount of light."
-        f" A grayscale image due to its nature conveys less information therefore the model is expected to require"
-        f" less computational power to train. \n\n However, feeding a model with an RGB image or convert that image to grayscale "
-        f" depends on the nature of the images and the information conveyd by the colour."
+        f""
     )
     st.warning(
-        f"Although coloured imges performed better in terms of accuracy, the difference in trainable parameters was marginal,"
-        f" hence providing little to no benefits to the computational cost. "
+        f""
+        f""
     )
-    model_perf_rgb = plt.imread(f"")
-    st.image(model_perf_rgb, caption='')
-    model_perf_gray = plt.imread(f"")
-    st.image(model_perf_gray, caption='') 
+    
 
     st.write(
         f"For additional information, please visit and **read** the "
